bb1e10acd0c786fab69bc912db3f7f735e9c68f9
Anders Lundström <anders@urkraft.se>
2016-06-03 19:19:56 +0200

    less imports paths on types/store.ex

--- web/graphql/types/store.ex
+++ web/graphql/types/store.ex
@@ -2,17 +2,15 @@ defmodule App.Type.Store do
 
   alias GraphQL.Schema
   alias GraphQL.Type.ObjectType
-
+  alias GraphQL.Type.ID
+  alias GraphQL.Type.NonNull
   alias GraphQL.Relay.Node
   alias GraphQL.Relay.Connection
   alias GraphQL.Relay.Mutation
-  import RethinkDB.Query, only: [table: 1]
-  alias RethinkDB.Query
+  import RethinkDB.Lambda, only: [lambda: 1]
+  import RethinkDB.Query, only: [table: 1, order_by: 2, desc: 1, filter: 2, match: 2]
   @type_string %{type: %GraphQL.Type.String{}}
-  import RethinkDB.Lambda
 
-  alias GraphQL.Type.ID
-  alias GraphQL.Type.NonNull
 
   def get do
     %ObjectType{
@@ -24,15 +22,17 @@ defmodule App.Type.Store do
           args: Map.merge(Connection.args, %{query: @type_string}),
           resolve: fn ( _, args , _ctx) ->
             query = table("links")
-              |> Query.filter( lambda fn(link) ->  Query.match(link[:title],args[:query]) end)
-              |> Query.order_by(Query.desc("timestamp"))
+              |> filter(lambda fn(link) ->
+                match(link[:title],
+                args[:query]) end)
+              |> order_by(desc("timestamp"))
               |> DB.run
               |> DB.handle_graphql_resp
           Connection.List.resolve(query, args)
           end
         }
       },
-      interfaces: [App.PublicSchema.node_interface]
+      interfaces: [App.Utils.node_interface]
     }
   end
 end
