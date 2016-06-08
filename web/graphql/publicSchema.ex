99895a14ebee336401b6fe0848b2cf9f99e508b9
Anders Lundström <anders@urkraft.se>
2016-06-08 09:19:51 +0200

    alias Util

--- web/graphql/publicSchema.ex
+++ web/graphql/publicSchema.ex
@@ -1,14 +1,16 @@
 defmodule App.PublicSchema do
   alias GraphQL.Schema
   alias GraphQL.Type.ObjectType
+  alias App.Utils
+
 
   def schema do
     %Schema{
       query: %ObjectType{
         name: "Query",
         fields: %{
-          node: App.Utils.node_field,
-          store: App.Utils.store_field
+          node: Utils.node_field,
+          store: Utils.store_field
         }
       },
       mutation: App.Mutation.tree
